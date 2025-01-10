fcd1cea029500bf81c2c1a5509351271e33e655d
Mayza Ester <130400582+maisazs@users.noreply.github.com>
2025-01-09 23:37:59 -0300

    nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
 
         'gray-1': '#9E9E9E',
         'gray-2': '#050505',
+        'gray-3': '#101010',
+        'gray-4': '#111111',
+        'gray-5': '#696969',
 
         'primary-1': '#D8FA2D',
       },
